f0c4cbd793690bbe119266a672afd6926a153067
Sulucnal <100075515+Sulucnal@users.noreply.github.com>
2024-10-29 12:06:30 +0100

    Added a main scene and a scenes manager

---
+++ autoloads/scenes_manager.gd
@@ -0,0 +1,35 @@
+extends Node
+
+
+##An autoload to add and remove 2D and UI scenes to the main one. 
+
+
+enum SceneType {SCENE_2D, SCENE_UI}
+
+
+@onready var main : Main = get_tree().root.get_child(-1)
+
+
+##Adds a scene to the main one.
+func add_scene(scene_path : String, scene_type : SceneType) -> void:
+	match scene_type:
+		SceneType.SCENE_2D:
+			var scene : Node2D = load(scene_path).instantiate()
+			main.world_parent.add_child(scene)
+		SceneType.SCENE_UI:
+			var scene : Control = load(scene_path).instantiate()
+			main.ui_parent.add_child(scene)
+
+
+##Removes a scene from the main one. Providing a scene type isn't necessary but it is recommended.
+func remove_scene(scene_name : String, scene_type : SceneType = -1) -> void:
+	match scene_type:
+		SceneType.SCENE_2D:
+			var node_to_remove : Node2D = main.world_parent.find_child(scene_name, false)
+			node_to_remove.queue_free()
+		SceneType.SCENE_UI:
+			var node_to_remove : Node2D = main.world_parent.find_child(scene_name, false)
+			node_to_remove.queue_free()
+		_:
+			var node_to_remove := main.find_child(scene_name)
+			node_to_remove.queue_free()
---
+++ scenes/main/main.gd
@@ -0,0 +1,10 @@
+extends Node
+class_name Main
+
+
+##The main scene where everything happens. Scenes can be added or removed from it 
+##using the ScenesManager autoload.
+
+
+@onready var world_parent: Node2D = $WorldParent
+@onready var ui_parent: Control = $UIParent
