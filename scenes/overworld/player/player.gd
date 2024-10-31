0dc043060454b78e76d4b8811293f0ddd04035a6
Altarax <altarax@yahoo.com>
2024-10-31 12:16:35 +0100

    facing_direction property

--- scenes/overworld/player/player.gd
+++ scenes/overworld/player/player.gd
@@ -1,2 +1,8 @@
 extends StaticBody2D
 class_name Player
+
+var _facing_direction : Vector2i = Vector2i(0, 0)
+var facing_direction : Vector2i = _facing_direction :
+	get: return _facing_direction
+	set(dir):
+		_facing_direction = dir
