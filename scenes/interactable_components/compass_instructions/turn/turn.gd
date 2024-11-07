fc1df1d574150a8f60bd5bdaabef1e919a41007d
Altarax <altarax@yahoo.com>
2024-11-07 12:37:22 +0100

    Added random turn and walk for compass

--- scenes/interactable_components/compass_instructions/turn/turn.gd
+++ scenes/interactable_components/compass_instructions/turn/turn.gd
@@ -3,8 +3,12 @@ extends Compass_Instruction
 
 @export var direction : Vector2i
 @export var duration : float
+@export var random_direction : bool
 
 
 func consume(_object_to_instruct) -> void:
+	if random_direction:
+		direction = [Vector2i(0,-1), Vector2i(0,1), Vector2i(-1, 0), Vector2i(1, 0)][randi_range(0, 3)]
+
 	_object_to_instruct.moveable_component.turn(direction)
 	_object_to_instruct.current_state.state_machine.transition_to("Idle", {"duration" : duration, "callback" : after_consumed_callback})
