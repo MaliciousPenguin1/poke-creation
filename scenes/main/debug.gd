481e0cacb62ae3c618312d49b25659ca6191868a
Sulucnal <100075515+Sulucnal@users.noreply.github.com>
2024-11-14 22:50:53 +0100

    Added a bugged dialog system
    The current dialog system can't handle more than two lines of text.

--- scenes/main/debug.gd
+++ scenes/main/debug.gd
@@ -10,5 +10,12 @@ extends Node
 
 func _ready() -> void:
 	await owner.ready
-	ScenesManager.add_scene("res://scenes/ui/main_menu/main_menu.tscn", ScenesManager.SceneType.UI)
+	#ScenesManager.add_scene("res://scenes/ui/main_menu/main_menu.tscn", ScenesManager.SceneType.UI)
+	ScenesManager.add_scene("res://scenes/overworld/worlds/maps/debug_town_map.tscn", ScenesManager.SceneType.WORLD)
+	ScenesManager.add_scene("res://scenes/overworld/player/Player.tscn", ScenesManager.SceneType.ENTITY)
+	custom_camera.reparent(GlobalVar.player)
+	custom_camera.move_to_parent()
+	
+	await get_tree().create_timer(1).timeout
+	Utils.show_dialog("BUCKET_ALCHEMY")
 	queue_free()
--- scripts/global_var.gd
+++ scripts/global_var.gd
@@ -3,8 +3,9 @@ class_name GlobalVar
 
 
 static var can_pause : bool = true
+static var in_game : bool = false
 static var reserved_tiles : Dictionary
 static var player : Player
 
-
 static var text_parser : TextParser
+static var time_speed : float = 1.0
