882436b827c34f2e389d13ae2555fdf56deae2c4
Sulucnal <100075515+Sulucnal@users.noreply.github.com>
2024-11-21 18:26:44 +0100

    Added the cursor and auto-hiding of the difficulty button

---
+++ scenes/ui/options_menu/options_menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+@onready var difficulty_button: TextureButton = $CanvasLayer/Background/VBoxContainer/DifficultyButton
+
+
+func _ready() -> void:
+	if GlobalVar.in_game:
+		difficulty_button.show()
