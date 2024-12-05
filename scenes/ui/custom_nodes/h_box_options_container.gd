8ec52fa2fab6c1954d1ccf3974e42c30b294b92d
Sulucnal <100075515+Sulucnal@users.noreply.github.com>
2024-12-05 16:11:27 +0100

    Rewrote the way the options menu works

--- scenes/ui/custom_nodes/h_box_options_container.gd
+++ scenes/ui/custom_nodes/h_box_options_container.gd
@@ -3,71 +3,58 @@ class_name HBoxOptionsContainer
 
 
 @onready var button_left: TextureSoundButton = $ButtonLeft
-@onready var options_list: Control = $OptionsList
+@onready var options_label: Control = $OptionsLabel
 @onready var button_right: TextureSoundButton = $ButtonRight
 
 
+#Once Godot 4.4 is release, use @export var values : Dictionary[String, Variant] where the key 
+#is what should be displayed in the label
+@export var values : Dictionary
+@export var keys_array : Array[String]
 @export var infinite_scroll : bool = true
 
 
-var current_option : Node :
+var current_option_index : int = 0 :
 	set(value):
-		current_option = value
-		_set_current_option()
-var last_option : Node
-var current_option_index : int = 0
-
-var selected : bool = false :
-	set(value):
-		selected = value
-		_on_selected()
-
-
-func _set_current_option() -> void:
-	_set_buttons_state()
-	current_option_index = current_option.get_index()
+		current_option_index = value
+		_on_current_option_index_changed()
+var selected : bool = false
 
 
 func _set_buttons_state() -> void:
 	if infinite_scroll:
 		return
 	
-	if current_option == options_list.get_child(0):
+	if current_option_index == 0:
 		button_left.disabled = true
 	else:
 		button_left.disabled = false
 	
-	if current_option == options_list.get_child(options_list.get_child_count() - 1):
+	if current_option_index == values.size() - 1:
 		button_right.disabled = true
 	else:
 		button_right.disabled = false
 
 
 func _on_button_left_pressed() -> void:
-	current_option_index -= 1
-	if current_option_index < 0:
-		current_option_index = options_list.get_child_count() - 1
+	if current_option_index - 1 < 0:
+		current_option_index = values.size() - 1
+	else:
+		current_option_index -= 1
 	
-	last_option = current_option
-	current_option = options_list.get_child(current_option_index)
-	
-	last_option.hide()
-	current_option.show()
+	options_label.text = keys_array[current_option_index]
 
 
 func _on_button_right_pressed() -> void:
-	current_option_index += 1
-	if current_option_index == options_list.get_child_count():
+	if current_option_index + 1 == values.size():
 		current_option_index = 0
+	else:
+		current_option_index += 1
 	
-	last_option = current_option
-	current_option = options_list.get_child(current_option_index)
-	
-	last_option.hide()
-	current_option.show()
+	options_label.text = keys_array[current_option_index]
 
 
-func _on_selected() -> void:
+func _on_current_option_index_changed() -> void:
 	pass
 
 
--- scenes/ui/options_menu/settings_h_box_options_container.gd
+++ scenes/ui/options_menu/settings_h_box_options_container.gd
@@ -4,7 +4,6 @@ class_name SettingsHBoxOptionsContainer
 
 @export var cfg_section : String
 @export var cfg_key : String
-@export var values : Array
 
 @export var section_name : String
 @export var section_description: String
@@ -17,19 +16,18 @@ func _ready() -> void:
 	
 	Settings.check_config_file()
 	var current_value : Variant = Settings.config_file.get_value(cfg_section, cfg_key)
-	var current_value_index : int = values.find(current_value)
-	current_option = options_list.get_child(current_value_index)
-	current_option.show()
+	var current_key_string : String = values.find_key(current_value)
+	options_label.text = current_key_string
 
 
-func _set_current_option() -> void:
+func _on_current_option_index_changed() -> void:
 	if current_option_index >= values.size():
 		push_error("The option selected isn't part of the values available.")
 		return
 	
 	super()
-	Settings.config_file.set_value(cfg_section, cfg_key, values[current_option_index])
-	Settings.change_setting(cfg_key, values[current_option_index])
+	Settings.config_file.set_value(cfg_section, cfg_key, values[keys_array[current_option_index]])
+	Settings.change_setting(cfg_key, values[keys_array[current_option_index]])
 
 
 func _on_selected() -> void:
